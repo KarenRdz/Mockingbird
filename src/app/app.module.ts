e8fafd834127a571fe5ad5e7d908bb77340f64d0
Rodriguez Acosta <k.rodriguez.acosta@accenture.com>
2019-02-22 10:10:35 -0600

    create reusable nav and added to the home and parent-sesion component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { Home } from './components/Home/home.component'
 import { ParentSession } from './components/ParentSession/parent-session.component'
 import { appRoutes } from './routes/app.routes'
+import { NavBar } from './components/NavBar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Home,
-    ParentSession
+    ParentSession,
+    NavBar
   ],
   imports: [
     BrowserModule,
